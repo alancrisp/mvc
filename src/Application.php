56214a8ddf79cd340fd07a86ed14248a1b925bf6
Alan Crisp <git@alancrisp.net>
2015-05-03 14:25:46 +0100

    Execute controller and return response

--- src/Application.php
+++ src/Application.php
@@ -2,12 +2,14 @@
 namespace SimpleMvc;
 
 use SimpleMvc\Http\RequestInterface;
+use SimpleMvc\Http\ResponseInterface;
 use SimpleMvc\Router\RouterInterface;
 
 class Application
 {
     const EXCEPTION_NO_ROUTE_MATCH = 5001;
     const EXCEPTION_UNKNOWN_CONTROLLER = 5002;
+    const EXCEPTION_NO_RESPONSE = 5003;
 
     private $router;
     private $controllers;
@@ -33,6 +35,15 @@ class Application
             ), self::EXCEPTION_UNKNOWN_CONTROLLER);
         }
 
-        // @todo execute controller and return response
+        $controller = $this->controllers[$controllerName];
+        $response = call_user_func($controller, $request);
+        if (!$response instanceof ResponseInterface) {
+            throw new \Exception(sprintf(
+                'Controller \'%s\' did not provide a response',
+                $controllerName
+            ), self::EXCEPTION_NO_RESPONSE);
+        }
+
+        return $response;
     }
 }
--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -3,6 +3,7 @@ namespace SimpleMvcTest;
 
 use SimpleMvc\Application;
 use SimpleMvc\Http\Request;
+use SimpleMvc\Http\Response;
 use SimpleMvc\Router\Route\Literal;
 use SimpleMvc\Router\Router;
 
@@ -16,7 +17,10 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
     {
         $this->router = $this->getMock('SimpleMvc\Router\RouterInterface');
         $this->route = $this->getMock('SimpleMvc\Router\Route\RouteInterface');
-        $this->application = new Application($this->router, []);
+        $this->application = new Application($this->router, [
+            'hello' => function () { return new Response(200, 'Hello!'); },
+            'no-response' => function () {},
+        ]);
     }
 
     public function testThrowsExceptionOnNoRouteMatch()
@@ -29,10 +33,38 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testThrowsExceptionOnUnknownController()
     {
-        $this->router->method('match')->willReturn($this->route);
-        $this->route->method('getControllerName')->willReturn('invalidController');
+        $this->givenRouterMatchesRoute();
+        $this->givenRouteProvidesControllerName('invalidController');
         $request = new Request('/');
         $this->setExpectedException('Exception', '', Application::EXCEPTION_UNKNOWN_CONTROLLER);
         $this->application->dispatch($request);
+    }
+
+    public function testThrowsExceptionWhenControllerDoesNotProvideResponse()
+    {
+        $this->givenRouterMatchesRoute();
+        $this->givenRouteProvidesControllerName('no-response');
+        $this->setExpectedException('Exception', '', Application::EXCEPTION_NO_RESPONSE);
+        $request = new Request('/');
+        $this->application->dispatch($request);
+    }
+
+    public function testReturnsResponse()
+    {
+        $this->givenRouterMatchesRoute();
+        $this->givenRouteProvidesControllerName('hello');
+        $request = new Request('/hello');
+        $response = $this->application->dispatch($request);
+        $this->assertInstanceOf('SimpleMvc\Http\ResponseInterface', $response);
+    }
+
+    private function givenRouterMatchesRoute()
+    {
+        $this->router->method('match')->willReturn($this->route);
+    }
+
+    private function givenRouteProvidesControllerName($name)
+    {
+        $this->route->method('getControllerName')->willReturn($name);
     }
 }
